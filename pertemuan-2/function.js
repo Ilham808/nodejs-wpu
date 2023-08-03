d79dd9425b0458e0185710b7460633c0f049c49f
Ilham Budiawan Sitorus <60646465+Ilham808@users.noreply.github.com>
2023-08-03 15:18:29 +0700

    export objek dan class

--- pertemuan-2/function.js
+++ pertemuan-2/function.js
@@ -5,6 +5,22 @@ function mtkDasar(number1, number2){
 
 const PI = 3.14;
 
+const mahasiswa = {
+    nama: 'Ilham Budiawan Sitorus',
+    umur: 20,
+    cetakNama() {
+        return `Halo, Nama Saya ${this.nama} dan umur saya ${this.umur} tahun`;
+    }
+};
+
+class Mobil {
+    constructor() {
+        console.log('Lagi buat class mobil');
+    }
+}
+
 // module.exports = mtkDasar;
 module.exports.mtkDasar = mtkDasar;
 module.exports.PI = PI;
+module.exports.mahasiswa = mahasiswa;
+module.exports.Mobil = Mobil;
--- pertemuan-2/index.js
+++ pertemuan-2/index.js
@@ -2,3 +2,6 @@ const functionFile = require('./function'); // local module
 
 console.log(functionFile.mtkDasar(5, 10));
 console.log(functionFile.PI);
+console.log(functionFile.mahasiswa);
+console.log(functionFile.mahasiswa.cetakNama());
+console.log(new functionFile.Mobil());
